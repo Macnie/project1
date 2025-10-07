2bc84a88114850dc55575d444c0bce0676ae8d18
Macnie <m_nayden@mail.ru>
2025-10-07 04:07:54 +0300

    Initial commit

--- LABA4.cpp
+++ LABA4.cpp
@@ -1,150 +1,108 @@
-
 #include <iostream>
 #include <cmath>
-#include <iomanip>
 
 using namespace std;
 
 int main()
 
-    // Zadanie 1
-    //{
-    //	int n = 0, d;
-    //	double a, b, h, y, y1, x, x1, ymax, ymin;
-    //	cout << "Enter 1, if you want check the result; 2 - write numbers: ";
-    //	cin >> d;
-    //	if ((!cin) || ((d < 1) || (d > 2)))
-    //	{
-    //		cout << "Neverniy vvod (vibirite ot 1 do 2)";
-    //		return 0;
-    //	}
-    //	else
-    //	{
-    //		if (d == 1)
-    //		{
-    //			a = -0.9;
-    //			b = 2.7;
-    //			h = 0.3;
-    //		}
-    //		else
-    //		{
-    //			cout << "Enter a: ";
-    //			cin >> a;
-    //			if (!cin)
-    //			{
-    //				cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
-    //				return 0;
-    //			}
-    //			cout << "Enter b: ";
-    //			cin >> b;
-    //			if (!cin)
-    //			{
-    //				cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
-    //				return 0;
-    //			}
-    //			cout << "Enter h: ";
-    //			cin >> h;
-    //			if (!cin)
-    //			{
-    //				cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
-    //				return 0;
-    //			}
-    //			if (a > b)
-    //			{
-    //				cout << "Neverniy vvod (a dozhno bit' men'she b)";
-    //				return 0;
-    //			}
-    //			if (h <= 0)
-    //			{
-    //				cout << "Neverniy vvod (shag dolzhen bit' bol'she nulya)";
-    //				return 0;
-    //			}
-    //		}
-    //	}
-    //	x = a;
-    //	ymax = ymin = y = (1 - x * x / 4) * cos(x) - (x / 2) * sin(x);
-    //	cout << "Results" << setw(25) << "Serial number: " << setw(25) << "X argument value: " << setw(25) << "Function value y: " << endl;
-    //	for (x = a; x <= b; x = x + h)
-    //	{
-    //		y = (1 - x * x / 4) * cos(x) - (x / 2) * sin(x);
-    //		if (y < ymin)
-    //		{
-    //			ymin = y;
-    //		}
-    //		else
-    //		{
-    //			if (y > ymax)
-    //			{
-    //				ymax = y;
-    //			}
-    //		}
-    //		n++;
-    //		cout << setw(25) << n << setw(25) << x << setw(25) << y << endl;
-    //	}
-    //	cout << "The maximum value of the function y: " << ymax << endl;
-    //	cout << "The minimum value of the function y: " << ymin << endl;
-    //	system("pause");
-    // }
+// Zadanie 1
+//{
+//	int i;
+//	double n, mini, minn, sum = 0;
+//	cout << "Enter n: ";
+//	cin >> n;
+//	if ((n < 1) || (n > floor(n)) || (!cin))
+//	{
+//		cout << "Neverniy vvod (n dolzno bit' natural'nim chislom)" << endl;
+//		return 0;
+//	}
+//	double* a = new double[n];
+//	cout << "Enter array character: " << endl;
+//	for (i = 0; i < n; i++)
+//	{
+//		cout << "[" << i << "]: ";
+//		cin >> a[i];
+//	}
+//	mini = a[0];
+//	minn = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		if (a[i] < mini)
+//		{
+//			mini = a[i];
+//			minn = i;
+//		}
+//	}
+//	if (minn == 0)
+//	{
+//		cout << "Min element perviy (symmy poschitet' nevozmozhno)";
+//		return 0;
+//	}
+//	for (i = 0; i < minn; i++)
+//	{
+//		sum += a[i];
+//	}
+//	cout << "Result is: " << sum << endl;
+//	delete[] a;
+//	system("pause");
+// }
 
-    // Zadanie 2
-    //{
-    //	double s, r, y, a, b, h, x, raz;
-    //	int k, n, z;
-    //	cout << "Enter a: ";
-    //	cin >> a;
-    //	if (!cin)
-    //	{
-    //		cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
-    //		return 0;
-    //	}
-    //	cout << "Enter b: ";
-    //	cin >> b;
-    //	if (!cin)
-    //	{
-    //		cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
-    //		return 0;
-    //	}
-    //	cout << "Enter h: ";
-    //	cin >> h;
-    //	if (!cin)
-    //	{
-    //		cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
-    //		return 0;
-    //	}
-    //	cout << "Enter n: ";
-    //	cin >> n;
-    //	if (!cin)
-    //	{
-    //		cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
-    //		return 0;
-    //	}
-    //	if (a > b)
-    //	{
-    //	    cout << "Neverniy vvod (a dozhno bit' men'she b)";
-    //	    return 0;
-    //	}
-    //	if (h <= 0)
-    //	{
-    //		cout << "Neverniy vvod (shag dolzhen bit' bol'she nulya)";
-    //		return 0;
-    //	}
-    //	z = 0;
-    //	x = a;
-    //	k = 0;
-    //	cout << "Results" << setw(20) << "Serial number: " << setw(20) << "X argument value: " << setw(20) << "Function value y: " << setw(20) << "Sum value: " << setw(20) << "Difference value: " << endl;
-    //	for (a; a <= b; a = a + h)
-    //	{
-    //		x = a;
-    //		r = s = 1;
-    //		for (k = 1; k <= n; k++)
-    //		{
-    //			r = -r * x * x / ((2*k)*(2*k-1));
-    //			s += r;
-    //		}
-    //		y = cos(x);
-    //		raz = abs(y - s);
-    //		z++;
-    //		cout << setw(20) << z << setw(20) << a << setw(20) << y << setw(20) << s << setw(20) << raz << endl;
-    //	}
-    //	system("pause");
-    // }
+// Zadanie 2
+{
+    int i, notr = 0, d, n;
+    cout << "Enter n: ";
+    cin >> n;
+    if ((n < 1) || (!cin))
+    {
+        cout << "Neverniy vvod (n dolzno bit' natural'nim chislom)" << endl;
+        return 0;
+    }
+    int *a = new int[n];
+    cout << "Enter 1 if you want to write array numbers, 2 - random array numbers: ";
+    cin >> d;
+    if (d != 1 && d != 2)
+    {
+        cout << "Neverniy vvod (vibirite 1 ili 2)";
+        return 0;
+    }
+    else
+    {
+        if (d == 1)
+        {
+            cout << "Enter array characters: " << endl;
+            for (i = 0; i < n; i++)
+            {
+                cout << "[" << i << "]: ";
+                cin >> a[i];
+            }
+        }
+        else
+        {
+            cout << "Array characters: " << endl;
+            for (i = 0; i < n; i++)
+            {
+                a[i] = -n + rand() % (2 * n + 1);
+                cout << "[" << i << "]: " << a[i] << endl;
+            }
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] < 0)
+        {
+            for (notr = i; notr < n - 1; notr++)
+            {
+                a[notr] = a[notr + 1];
+            }
+            n--;
+            i--;
+        }
+    }
+    cout << "New array is: " << endl;
+    for (i = 0; i < n; i++)
+    {
+        cout << "[" << i << "]: " << a[i] << endl;
+    }
+    delete[] a;
+    system("pause");
+}
