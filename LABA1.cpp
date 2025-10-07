b1882bd2713aaa2d4e96210b84cb57645118b2c9
Macnie <m_nayden@mail.ru>
2025-10-07 03:51:50 +0300

    Initial commit

---
+++ LABA1.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+    // Zadanie 1
+    //{
+    //	double x, a, b, pi, z1, z2, vv = 1;
+    //	pi = 3.1415926;
+    //	cout << "Enter x: ";
+    //	cin >> x;
+    //	a = cos(3. / 8 * pi - x / 4);
+    //	b = cos(11. / 8 * pi + x / 4);
+    //	z1 = a * a - b * b;
+    //	cout << "Resalt z1 is: " << z1 << endl;
+    //	z2 = (sqrt(2) / 2) * sin(x / 2);
+    //	cout << "Resalt z2 is: " << z2 << endl;
+    //     system("pause");
+    // }
+
+    // Zadanie 2
+    //{
+    //	double a, b, alfa, pi, z1, z2;
+    //	pi = 3.1415926;
+    //	cout << "Enter alfa (alfa should be a number): ";
+    //	cin >> alfa;
+    //	if (!cin)
+    //	{
+    //		cout << "Error!" << endl;
+    //		system("pause");
+    //	}
+    //	else
+    //	{
+    //		if ((1 + cos(4 * alfa) == 0) || (1 + cos(2 * alfa) == 0))
+    //		{
+    //			cout << "Wrong data entry!!!";
+    //			system("pause");
+    //		}
+    //		else
+    //		{
+    //			a = (sin(4 * alfa)) / (1 + cos(4 * alfa));
+    //			b = (cos(2 * alfa)) / (1 + cos(2 * alfa));
+    //			z1 = a * b;
+    //			cout << "Resalt z1 is: " << z1 << endl;
+    //			z2 = 1 / tan(3. / 2 * pi - alfa);
+    //			cout << "Resalt z2 is: " << z2 << endl;
+    //			system("pause");
+    //		}
+    //	}
+    // }
+
+    // Zadanie 3
+    //{
+    //	double a, b, c, d, x, y, z, w;
+    //	cout << "Enter 1, if you want check result, 2 - write your numbers: ";
+    //	cin >> d;
+    //	if (d == 1)
+    //	{
+    //		x = 0.4e4;
+    //		y = -0.875;
+    //		z = -0.475e-3;
+    //	}
+    //	else
+    //	{
+    //		cout << "Enter x: ";
+    //		cin >> x;
+    //		cout << "Enter y: ";
+    //		cin >> y;
+    //		cout << "Enter z: ";
+    //		cin >> z;
+    //		if (!cin)
+    //		{
+    //			cout << "Error!";
+    //			return 0;
+    //		}
+    //	}
+    //	a = abs(cos(x) - cos(y));
+    //	b = 1 + 2 * sin(y) * sin(y);
+    //	c = 1 + z + z * z / 2 + z * z * z / 3 + z * z * z * z / 4;
+    //	w = pow(a, b) * c;
+    //	w = ceil(w * 10000) / 10000;
+    //	cout << "Result is: " << w << endl;
+    //	system("pause");
+    // }
