b423cf6f8fbad90695fb168b0a503639baeb4858
Macnie <m_nayden@mail.ru>
2025-10-07 04:03:34 +0300

    Initial commit

---
+++ LABA2.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+    // Zadanie 1
+    //{
+    //	double y, a, b, x, z;
+    //	cout << "Enter z: ";
+    //	cin >> z;
+    //	if (!cin)
+    //	{
+    //		cout << "Error!";
+    //	}
+    //	else
+    //	{
+    //		if (z < 1)
+    //		{
+    //			x = z * z * z + 0.2;
+    //			cout << "Calculations are made for: " << "x = z * z * z + 0.2" << endl;
+    //		}
+    //		else
+    //		{
+    //			x = z + log(z);
+    //			cout << "Calculations are made for: " << "x = z + log(z)" << endl;
+    //		}
+    //		a = cos(x*x);
+    //		b = sin(x*x*x);
+    //		y = a * a * a + b * b;
+    //		cout << "Result is: " << y << endl;
+    //	}
+    //	system("pause");
+    // }
+
+    // Zadanie 2
+    //{
+    //	double y, a, b, x, z, cosinus, sinus, fi;
+    //	int d;
+    //	cout << "Enter z: ";
+    //	cin >> z;
+    //	if (!cin)
+    //	{
+    //		cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
+    //		return 0;
+    //	}
+    //	else
+    //	{
+    //		if (z < 1)
+    //		{
+    //			x = z * z * z + 0.2;
+    //			cout << "Calculations are made for: " << "x = z * z * z + 0.2" << endl;
+    //		}
+    //		else
+    //		{
+    //			x = z + log(z);
+    //			cout << "Calculations are made for: " << "x = z + log(z)" << endl;
+    //		}
+    //	}
+    //	cout << "Choose which variable to calculate (1 for x=2*x, 2 for x=x*x, 3 for x=x/3): ";
+    //	cin >> d;
+    //	if (((d<1) || (d>3)) || (!cin))
+    //	{
+    //		cout << "Error! (You should chouse from 1 to 3)";
+    //		return 0;
+    //	}
+    //	else
+    //	{
+    //		switch (d)
+    //		{
+    //		case 1: fi = 2 * x; cout << "Calculations are made for: fi(x)=2*x"<<endl; break;
+    //		case 2: fi = x * x; cout << "Calculations are made for: fi(x)=x*x"<<endl; break;
+    //		case 3: fi = x / 3; cout << "Calculations are made for: fi(x)=x/3"<<endl; break;
+    //		}
+    //	}
+    //	cout << "Enter a: ";
+    //	cin >> a;
+    //	if (!cin)
+    //	{
+    //		cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
+    //		return 0;
+    //	}
+    //	else
+    //	{
+    //		cout << "Enter b: ";
+    //		cin >> b;
+    //		if (!cin)
+    //		{
+    //			cout << "Neverniy vvod (dolzhno bit' chislovoe znachenie)";
+    //			return 0;
+    //		}
+    //	}
+    //	cosinus = cos(x*x);
+    //	sinus = sin(x*x*x);
+    //	y = 2 * a*cosinus*cosinus*cosinus + sinus * sinus - b * fi;
+    //	cout << "Result is: " << y << endl;
+    //	system("pause");
+    // }
